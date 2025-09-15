57b07d2a555e7d856f6ede6d8a606d98b959651c
joaoalbertorsc <jaoalbertorsc@gmail.com>
2025-09-14 22:43:50 -0300

    feat: Create main application class for CNPJ Finder

---
+++ src/main/java/com/cnpjfinder/ConsultaCnpjApplication.java
@@ -0,0 +1,11 @@
+package com.cnpjfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaCnpjApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsultaCnpjApplication.class, args);
+    }
+}
